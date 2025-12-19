f1036e2eb4af1a3b87c0ccdbe5e13deaa6ec01c8
Lucas Bezerra Marques de Souza <lucasbezerra527@gmail.com>
2025-12-19 10:19:43 -0300

    feat: Enhance User admin interface with custom fieldsets and display options

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -9,3 +9,19 @@ class UserAdmin(BaseUserAdmin):
     """Admin para o modelo User."""
 
     ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Informações Pessoais", {"fields": ("name",)}),
+        ("Permissões", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "name", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "name", "is_staff", "is_active")
+    search_fields = ("email", "name")
